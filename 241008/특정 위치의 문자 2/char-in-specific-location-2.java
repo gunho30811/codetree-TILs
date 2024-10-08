fab01d921bf4e4b9160589ccd3dff4aabb220516
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2024-10-08 22:22:30 +0900

    Title: 특정 위치의 문자 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String ars=sc.nextLine();
+        String [] array=ars.split(" ");
+        System.out.println(array[1]+" "+array[4]+" "+array[7]+" ");
+        // 여기에 코드를 작성해주세요.
+    }
+}
